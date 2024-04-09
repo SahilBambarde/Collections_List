2eb6efa5f8768a285708f022a7d7f252051d6834
sahil_bambarde <sahil_bambarde>
2024-04-09 14:27:16 +0530

    Initial commit

---
+++ CursorsEnumDemo.java
@@ -0,0 +1,14 @@
+public class CursorsEnumDemo {
+    /*
+    * Cursors
+    * An Indicator used to show the current position for respective object or element
+    *
+    * Four Types of Cursors
+    * Enumeration
+    * Iterator
+    * ListIterator
+    * Spliterator
+    * */
+
+
+}
---
+++ VectorDemo.java
@@ -0,0 +1,21 @@
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args){
+        /*
+        * Allows Duplicate elements
+        * Insertion Order is preserved
+        * Accepts heterogeneous objects
+        * CloneAble
+        * RandomAccess
+        * Thread-safe
+        * */
+
+        Vector c = new Vector();
+        //initial size of a vector is 10.
+        //after this if will increate the size to double
+
+        Vector vec = new Vector(60);
+
+    }
+}
